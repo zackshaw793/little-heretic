b56daeed94a2bb9369e1135ac793b1f798146025
zackshaw793 <zackary.shaw@gmail.com>
2023-11-30 12:35:19 -0500

    fixed build

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> 
-    <BrowserRouter>
+    <BrowserRouter basename={'/little-heretic/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
